8b2eac84f7a79608688d177ae0b2cfaab190d54d
benimaru73 <fitahinaleong@gmail.com>
2025-03-22 10:31:06 +0300

    initialize

---
+++ src/main/java/com/itu/eval_spring/EvalSpringApplication.java
@@ -0,0 +1,13 @@
+package com.itu.eval_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvalSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvalSpringApplication.class, args);
+    }
+
+}
